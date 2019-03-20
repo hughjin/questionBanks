01b0db1e5881326fc22f987144fe589bd5748fc1
hughjin <1369766951@qq.com>
2019-03-20 14:23:38 +0800

    A+B2

--- src/com/question/bank/leedcode/Solution.java
+++ src/com/question/bank/leedcode/Solution.java
@@ -1,8 +1,6 @@
 package com.question.bank.leedcode;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -21,20 +19,24 @@ class Solution {
 		for (int i : result) {
 			System.out.println(i);
 		}
+		int[] result2 = Solution.twoSum2(nums, target);
+		for (int i : result2) {
+			System.out.println(i);
+		}
 	}
 
 	public static int[] twoSum(int[] nums, int target) {
 		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i < nums.length; i++) {
-			for (int k = i+1; k < nums.length; k++) {
-				if (nums[k] == target - nums[i] ) {
+			for (int k = i + 1; k < nums.length; k++) {
+				if (nums[k] == target - nums[i]) {
 					map.put(i, k);
-					System.out.println(nums[i]+"+"+nums[k]);
+					System.out.println(nums[i] + "+" + nums[k]);
 				}
 			}
 		}
 
-		int[] result = new int[map.size()*2];
+		int[] result = new int[map.size() * 2];
 		int count = 0;
 		for (Entry<Integer, Integer> entry : map.entrySet()) {
 			result[count] = entry.getKey();
@@ -47,16 +49,12 @@ class Solution {
 
 	public static int[] twoSum2(int[] nums, int target) {
 		int[] result = new int[2];
-		int count = 0;
 		for (int i = 0; i < nums.length; i++) {
-			int j = nums[i];
-			int res = target - j;
-			for (int k = 0; k < nums.length; k++) {
-				int m = nums[k];
-				if (m == res && i != k) {
-					result[count] = i;
-					result[count++] = k;
-					break;
+			for (int k = i + 1; k < nums.length; k++) {
+				if (nums[k] + nums[i] == target) {
+					result[0] = i;
+					result[1] = k;
+					return result;
 				}
 			}
 		}
